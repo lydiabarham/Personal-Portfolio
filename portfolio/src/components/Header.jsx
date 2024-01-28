1eb5c87636868a9ad2077601f100d802e68a0cd7
lydiabarham <lcbarham9@gmail.com>
2024-01-28 13:45:24 +0000

    add initial styling to jumbotron header

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './styleHeader.css'
 
 function Header() {
   return (
-    <h1 className="header"> Lydia Barham's Portfolio
+    <div className="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-4 header"> Lydia Barham
     </h1>
+    <p className="lead">From communications to coding</p>
+  </div>
+</div>
   );
 }
 
--- portfolio/src/components/pages/ProjectGallery/ProjectGallery.jsx
+++ portfolio/src/components/pages/ProjectGallery/ProjectGallery.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import ProjectCard from './Projects/ProjectCard';
 import projectData from './Projects/projects.json';
-// import movieImg from "portfolio/public/images/movie.png"
-// import passwordImg from "./Projects./images/password.png";
-// import portfolioImg from "./Projects./images/portfolio.png";
-// import quizImg from "./Projects./images/quiz.png";
-// import schedulerImg from "./Projects./images/scheduler.png";
-// import weatherImg from "./Projects./images/weather.png";
 
 function ProjectGallery() {
   return (
