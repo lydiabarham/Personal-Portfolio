c26b509ee6b4046c6875802f4ad14d46cfee6134
lydiabarham <lcbarham9@gmail.com>
2024-01-29 12:22:26 +0000

    Add styling and additional content to project gallery

--- portfolio/src/components/pages/ProjectGallery/ProjectGallery.jsx
+++ portfolio/src/components/pages/ProjectGallery/ProjectGallery.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import ProjectCard from './Projects/ProjectCard';
 import projectData from './Projects/projects.json';
-import './styleProject.css'
+import './Projects/styleProject.css'
 
 function ProjectGallery() {
   return (
+    <>
     <div className='home-container'>
+      <h1>Project Gallery</h1>
+      <p>
+        Below are the projects I have worked on so far. Click on the image for links to the site and to read more about the skills, technologies and concepts used.
+      </p>
       {projectData.map((project) => (
         <ProjectCard
+          key={project.id} 
           id={project.id}
           name={project.name}
           image={project.image}
           deployed={project.deployed}
           gitHub={project.gitHub}
+          description={project.description}
         />
       ))}
     </div>
+    </>
   );
 }
 
--- portfolio/src/components/pages/ProjectGallery/Projects/ProjectCard.jsx
+++ portfolio/src/components/pages/ProjectGallery/Projects/ProjectCard.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import './styleProject.css'
 
 
-function ProjectCard(props) {
+function ShowContent(props) {
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} height="400" />
-      </div>
+    <>
       <div className="content">
         <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
+          <li style={{ textTransform: 'uppercase' }} className="project-name">
+            {props.name}
           </li>
           <li>
-            <Link to= {props.deployed}>Deployed</Link>
-          </li>
-          <li>
-            <Link to= {props.gitHub}>GitHub</Link> 
+            <Link target="_blank" className="link" to={props.deployed}>Deployed</Link> <Link target="_blank" className="link" to={props.gitHub}>GitHub</Link>
           </li>
         </ul>
       </div>
+      <div className='project-description'>
+        <p className="card-text"> {props.description} </p>
+      </div>
+    </>
+  )
+}
+
+function ProjectCard(props) {
+  const [showContent, setShowContent] = useState(false);
+
+  const toggleContent = () => {
+    setShowContent(!showContent);
+  };
+
+  return (
+    <div className="card" onClick={toggleContent}>
+      <div className="img-container">
+        <img className='center-fit' alt={props.name} src={props.image} />
+      </div>
+      {showContent && <ShowContent {...props} />}
     </div>
   );
 }
