e1ff55528b6a6337c08541514117e1d74e43e69e
lydiabarham <lcbarham9@gmail.com>
2024-02-01 13:49:27 +0000

    update header styling

--- portfolio/src/components/pages/Contact/PDFViewer.jsx
+++ portfolio/src/components/pages/Contact/PDFViewer.jsx
@@ -4,7 +4,7 @@ import "./styleContact.css";
 const PDFViewer = () => {
  return (
  <div>
- <iframe className="pdf-frame" src='..\..\public\ContactImages\Curriculum_Vitae_Lydia_Barham_24_01.pdf' width="100%" height="700px" />
+ <iframe className="pdf-frame" src='..\..\public\ContactImages\Curriculum_Vitae_Lydia_Barham_24_01.pdf' width="100%" height="450px" />
  </div>
  );
 };
