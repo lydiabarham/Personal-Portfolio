eff885c62412061a2d836b5575ead0a336f87f9b
lydiabarham <lcbarham9@gmail.com>
2024-02-22 14:47:31 +0000

    try again

--- portfolio/src/components/footer/Footer.jsx
+++ portfolio/src/components/footer/Footer.jsx
@@ -9,13 +9,13 @@ function Footer() {
       <li><a href="/" className="footer-contact-link">
         Profile
       </a></li>
-      <li><a href="/project" className="footer-contact-link">
+      <li><a href="project" className="footer-contact-link">
         Projects
       </a></li>
-      <li><a href="/about" className="footer-contact-link">
+      <li><a href="about" className="footer-contact-link">
         Past
       </a></li>
-      <li><a href="contact/*" className="footer-contact-link">
+      <li><a href="contact" className="footer-contact-link">
         Contact
       </a></li>
       </ul>
