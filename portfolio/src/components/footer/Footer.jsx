11daed2c9b63878fd64778beea9b43e68a15a345
lydiabarham <lcbarham9@gmail.com>
2024-02-22 14:45:23 +0000

    try again on link issue

--- portfolio/src/components/footer/Footer.jsx
+++ portfolio/src/components/footer/Footer.jsx
@@ -9,10 +9,10 @@ function Footer() {
       <li><a href="/" className="footer-contact-link">
         Profile
       </a></li>
-      <li><a href="project" className="footer-contact-link">
+      <li><a href="/project" className="footer-contact-link">
         Projects
       </a></li>
-      <li><a href="about" className="footer-contact-link">
+      <li><a href="/about" className="footer-contact-link">
         Past
       </a></li>
       <li><a href="contact/*" className="footer-contact-link">
